d323a68cf73b6cd87f67aa605e43a300b72b9ed5
sulaimanfawwazak <sulaimanfawwazak@gmail.com>
2024-12-01 18:48:25 -0800

    Feat: upload whole file to repo

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Responsi2Junpro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblNamaKaryawan = new System.Windows.Forms.Label();
+            this.lblDepKaryawan = new System.Windows.Forms.Label();
+            this.tbNamaKaryawan = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dgvTableData = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cbDepKaryawan = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTableData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNamaKaryawan
+            // 
+            this.lblNamaKaryawan.AutoSize = true;
+            this.lblNamaKaryawan.Location = new System.Drawing.Point(27, 87);
+            this.lblNamaKaryawan.Name = "lblNamaKaryawan";
+            this.lblNamaKaryawan.Size = new System.Drawing.Size(91, 13);
+            this.lblNamaKaryawan.TabIndex = 0;
+            this.lblNamaKaryawan.Text = "Nama Karyawan :";
+            // 
+            // lblDepKaryawan
+            // 
+            this.lblDepKaryawan.AutoSize = true;
+            this.lblDepKaryawan.Location = new System.Drawing.Point(27, 125);
+            this.lblDepKaryawan.Name = "lblDepKaryawan";
+            this.lblDepKaryawan.Size = new System.Drawing.Size(86, 13);
+            this.lblDepKaryawan.TabIndex = 1;
+            this.lblDepKaryawan.Text = "Dep. Karyawan :";
+            // 
+            // tbNamaKaryawan
+            // 
+            this.tbNamaKaryawan.Location = new System.Drawing.Point(125, 84);
+            this.tbNamaKaryawan.Name = "tbNamaKaryawan";
+            this.tbNamaKaryawan.Size = new System.Drawing.Size(120, 20);
+            this.tbNamaKaryawan.TabIndex = 2;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnInsert.Location = new System.Drawing.Point(43, 182);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(132, 32);
+            this.btnInsert.TabIndex = 4;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = false;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnEdit.Location = new System.Drawing.Point(245, 182);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(132, 32);
+            this.btnEdit.TabIndex = 5;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnDelete.Location = new System.Drawing.Point(440, 182);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(132, 32);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dgvTableData
+            // 
+            this.dgvTableData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTableData.Location = new System.Drawing.Point(27, 240);
+            this.dgvTableData.Name = "dgvTableData";
+            this.dgvTableData.Size = new System.Drawing.Size(545, 188);
+            this.dgvTableData.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(27, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 66);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cbDepKaryawan
+            // 
+            this.cbDepKaryawan.FormattingEnabled = true;
+            this.cbDepKaryawan.Location = new System.Drawing.Point(125, 122);
+            this.cbDepKaryawan.Name = "cbDepKaryawan";
+            this.cbDepKaryawan.Size = new System.Drawing.Size(299, 21);
+            this.cbDepKaryawan.TabIndex = 9;
+            this.cbDepKaryawan.Text = "Departemen";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 450);
+            this.Controls.Add(this.cbDepKaryawan);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dgvTableData);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.tbNamaKaryawan);
+            this.Controls.Add(this.lblDepKaryawan);
+            this.Controls.Add(this.lblNamaKaryawan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTableData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNamaKaryawan;
+        private System.Windows.Forms.Label lblDepKaryawan;
+        private System.Windows.Forms.TextBox tbNamaKaryawan;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dgvTableData;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox cbDepKaryawan;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace Responsi2Junpro
+{
+    public partial class Form1 : Form
+    {
+        private NpgsqlConnection conn;
+        string connstring = "Host=localhost;port=5432;Username=postgres;Password=informatika;Database=Perusahaan";
+
+        public DataTable dt;
+        public static NpgsqlCommand cmd;
+        private string sql = null;
+        private DataGridViewRow r;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            cbDepKaryawan.DataSource = new DepartemenDropDown[]
+            {
+                new DepartemenDropDown{ ID = 1, Name = "HR"},
+                new DepartemenDropDown{ ID = 2, Name = "Engineer"},
+                new DepartemenDropDown{ ID = 3, Name = "Developer"},
+                new DepartemenDropDown{ ID = 4, Name = "Product Manager"},
+                new DepartemenDropDown{ ID = 5, Name = "Finance"}
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                conn = new NpgsqlConnection(connstring);
+                conn.Open();
+
+                // Initialize DataGridView
+                dgvTableData.DataSource = null;
+                sql = "SELECT k.id_karyawan, k.nama, k.id_dep, d.kode_dep, d.nama_dep FROM karyawan AS k LEFT JOIN departemen AS d ON k.id_dep = d.id_dep";
+                cmd = new NpgsqlCommand(sql, conn);
+                dt = new DataTable();
+                NpgsqlDataReader rd = cmd.ExecuteReader();
+                dt.Load(rd);
+                dgvTableData.DataSource = dt;
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:" + ex.Message, "Select Fail!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                sql = @"INSERT INTO karyawan (id_karyawan, nama, id_dep) VALUES (_id_karyawan, _nama, _id_dep)";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_id_karyawan", tbNamaKaryawan.Text[0] + tbNamaKaryawan.Text[1] + tbNamaKaryawan.Text[2] + tbNamaKaryawan.Text[3] + tbNamaKaryawan.Text[4] + tbNamaKaryawan.Text[5]);
+                cmd.Parameters.AddWithValue("_nama", tbNamaKaryawan.Text);
+                cmd.Parameters.AddWithValue("_id_dep", tbNamaKaryawan.Text);
+                dt = new DataTable();
+                NpgsqlDataReader rd = cmd.ExecuteReader();
+                dt.Load(rd);
+                dgvTableData.DataSource = dt;
+
+                conn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:" + ex.Message, "Select failed!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            sql = "UPDATE TABLE karyawan SET nama = _nama, id_dep = _id_dep";
+            cmd = new NpgsqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("_nama", tbNamaKaryawan.Text);
+            cmd.Parameters.AddWithValue("_id_dep", (int)cbDepKaryawan.SelectedValue);
+
+            if ((int)cmd.ExecuteScalar() == 1)
+            {
+                MessageBox.Show("Data Karyawan berhasil di-update", "Well Done!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                conn.Close();
+                tbNamaKaryawan.Text = null;
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                sql = @"DELETE FROM karyawan WHERE nama = _nama AND dep_id = _dep_id";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_nama", tbNamaKaryawan.Text);
+                cmd.Parameters.AddWithValue("_dep_id", (int)cbDepKaryawan.SelectedValue);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:" + ex.Message, "Gagal menghapus data karyawan!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            if ((int)cmd.ExecuteScalar() == 1)
+            {
+                MessageBox.Show("Data Karyawan berhasil dihapus", "Well Done!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                conn.Close();
+                tbNamaKaryawan.Text = null;
+            }
+        }
+    }
+}
